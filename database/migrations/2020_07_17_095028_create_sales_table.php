a217a0797d9315d2f0ae9c1496850acb8821c27e
西垣雅矢 <nishigakimasaya@nishigakitadashiyanoMacBook-Pro.local>
2020-07-18 19:00:56 +0900

    make it better

--- database/migrations/2020_07_17_095028_create_sales_table.php
+++ database/migrations/2020_07_17_095028_create_sales_table.php
@@ -14,7 +14,7 @@ class CreateSalesTable extends Migration
     public function up()
     {
         Schema::create('sales', function (Blueprint $table) {
-            $table->id('product_id');
+            $table->id('product_id')->nullable();
             $table->string('product_color')->nullable();
             $table->string('sales_date')->nullable();
             $table->integer('sales_amount')->nullable();
