508d6553f890be57d0ad38e1394424927304edd9
西垣雅矢 <nishigakimasaya@nishigakitadashiyanoMacBook-Pro.local>
2020-07-24 17:07:02 +0900

    デフォルトのインデックス用文字列長を191に変更

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,16 +1,16 @@
 require('./bootstrap');
-// require('./chart');
-window.Vue = require('vue');
+require('./chart');
+// window.Vue = require('vue');
 // import Vue from 'vue'
 // import ExampleComponent from './components/ExampleComponent.vue'
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 // Vue.component('chart', require('./components/Chart.vue'));
 // Vue.component('header-component', require('./components/HeaderComponent.vue').default);
 
-const app = new Vue({
-    el: '#app'
-});
+// const app = new Vue({
+//     el: '#app'
+// });
 
 // var app = new Vue({
 //     el: '#app',
