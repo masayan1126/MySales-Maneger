813c45f8323b75a40e435393fa6cbcfb1c5890e6
西垣雅矢 <nishigakimasaya@nishigakitadashiyanoMacBook-Pro.local>
2020-07-24 19:46:16 +0900

    デフォルトのインデックス用文字列長を191に変更

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -37,7 +37,7 @@ class SalesController extends Controller
     public function drawChart(){
       $view = view('analytics');
       $all_sales = DB::table('sales')->get();
-      $view->sales_amount = $all_sales;
+      $view->all_sales = $all_sales;
       // return redirect(route('analytics',[
       //   'sales_amount' => $sales_amount,
       // ]));
