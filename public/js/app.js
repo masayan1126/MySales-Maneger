9f05ad5c39691e6d59a03e48c10a95e6a12f04e2
西垣雅矢 <nishigakimasaya@nishigakitadashiyanoMacBook-Pro.local>
2020-07-24 17:16:43 +0900

    デフォルトのインデックス用文字列長を191に変更

--- public/js/app.js
+++ public/js/app.js
@@ -19387,30 +19387,29 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
   !*** ./resources/assets/js/chart.js ***!
   \**************************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-(function () {
-  'use strict';
+"use strict";
 
-  var type = 'line';
-  var data = {
-    labels: ['2018-01', '2018-02', '2018-03', '2018-04', '2018-04'],
-    datasets: [{
-      label: 'type A',
-      data: [111, 222, 333, 444, 555]
-    }, {
-      label: 'type B',
-      data: [555, 444, 333, 222, 111]
-    }]
-  };
-  var options;
-  var ctx = $('#chart')[0].getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: type,
-    data: data,
-    options: options
-  });
-})();
+
+var type = 'line';
+var data = {
+  labels: ['2018-01', '2018-02', '2018-03', '2018-04', '2018-04'],
+  datasets: [{
+    label: 'type A',
+    data: [111, 222, 333, 444, 555]
+  }, {
+    label: 'type B',
+    data: [555, 444, 333, 222, 111]
+  }]
+};
+var options;
+var ctx = $('#chart')[0].getContext('2d');
+var myChart = new Chart(ctx, {
+  type: type,
+  data: data,
+  options: options
+});
 
 /***/ }),
 
