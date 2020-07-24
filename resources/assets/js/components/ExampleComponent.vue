ab2b6499cbc245ae721374c08d41f1e7a2b5fadc
西垣雅矢 <nishigakimasaya@nishigakitadashiyanoMacBook-Pro.local>
2020-07-24 15:05:46 +0900

    デフォルトのインデックス用文字列長を191に変更

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -18,17 +18,37 @@
 <script>
 import { Bar } from 'vue-chartjs'   
 
-    export default {
-        extends: Bar,
-        name: "example-component",
-        date(){
-          return{
-              test: 'test',
-          }
-        },
-        mounted() {
-            this.renderChart(data, options)
-            console.log('Component mounted.')
-        }
+export default {
+  extends: Bar,
+  name: "example-component",
+  data () {
+    return {
+      chartData: {
+      labels: ['January', 'February', 'March', 'April', 'May'],
+      datasets: [
+     {
+      label: 'Sample1',
+      data: [28, 20, 30, 40, 50],
+      backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(255, 159, 64, 0.2)'
+      ],
+      borderWidth: 1
+     }
+     ]
+   },
+  }
+},
+    components: {
+    Bar,
+},
+    mounted() {
+    this.renderChart(data, options)
+    console.log('Component mounted.')
     }
+  }
 </script>
