1cd0101aedcb80343563498294f33ae313c444c5
西垣雅矢 <nishigakimasaya@nishigakitadashiyanoMacBook-Pro.local>
2020-07-31 18:50:51 +0900

    update

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,33 +1,12 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>sale</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
-    <!-- ChartJS -->
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script> -->
-</head>
-@extends('layouts.app')
 <body>
-<!-- @include('parts.menu') -->
-<!-- <div id="app" class="container-fluid pt-4"> -->
-<h2>トップ画面</h2>
-<p>テスト</p>
-<!-- <canvas id="chart"></canvas> -->
-  <!-- <example-component></example-component>
-  <Chart></Chart> -->
-<!-- </div> -->
+  <div id="top">
+    @extends('layouts.app')
+    <h2>トップ画面</h2>
+    <p>テスト</p>
+  </div>
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-  <!-- <script src="js/app.js"></script> -->
-  <!-- <script src="{{ mix('js/app.js') }}"></script> -->
   <script src="{{ mix('js/app.js') }}" defer></script>
-<script>
-</script>
-
 </body>
 </html>
