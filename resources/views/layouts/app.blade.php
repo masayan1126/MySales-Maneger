2f48152719ece492925c7ab0334a34c5c3131118
西垣雅矢 <nishigakimasaya@nishigakitadashiyanoMacBook-Pro.local>
2020-08-01 13:55:21 +0900

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,7 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('ログアウト'') }}
                                     </a>
-                                    <a class="dropdown-item" href="{{ route('/index') }}"
+                                    <a class="dropdown-item" href="{{ route('logout) }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('売上一覧') }}
